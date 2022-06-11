1dd17de7369d517582994b78a5c8f9aa2ad93703
王世翰 <60683892+dimpDEV@users.noreply.github.com>
2022-06-11 18:57:52 +0700

    add label

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,24 +1,11 @@
 let generateDinh = () => {
   var dinh = document.getElementsByName("soDinh")[0].value;
   document.getElementsByClassName("getDinh")[0].style.display = "none";
-  var elements =
-    "<h2>Các đỉnh ngăn cách nhau bởi dấu <b>;</b> không dùng bừa dấu space.</h2><br>";
+  var elements = "<h2>Các đỉnh ngăn cách nhau bởi dấu <b>;</b> không dùng bừa dấu space.</h2><br>";
   for (var i = 1, j = 0; i <= dinh && j < dinh; i++, j++) {
-    elements +=
-      "<h3>Đỉnh " +
-      i +
-      "</h3><br>" +
-      '<label for="' +
-      j +
-      '">Các đỉnh nối với đỉnh ' +
-      i +
-      "</label><br>" +
-      '<input type="text" name="' +
-      j +
-      '"></input><br>';
+    elements += "<h3>Đỉnh " + i + "</h3><br>" + '<label for="' + j + '">Các đỉnh nối với đỉnh ' + i + "</label><br>" + '<input type="text" name="' + j + '"></input><br>';
   }
-  elements +=
-    '<select name="way"><option value="greedy" selected>Greedy</option><option value="welch">Welch Powell</option><option value="byLevel">Tô màu theo bậc</option></select><br>';
+  elements += '<label for="algorithm">Thuật toán: </label>' + '<select name="algorithm"><option value="greedy" selected>Greedy</option><option value="welch">Welch Powell</option><option value="byLevel">Tô màu theo bậc</option></select><br>';
   elements += '<input type="submit"><br>';
   document.getElementsByClassName("geneDinh")[0].innerHTML = elements;
   document.getElementsByClassName("geneDinh")[0].style.display = "";
